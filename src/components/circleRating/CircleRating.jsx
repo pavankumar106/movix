1a476ab99f00b366a8e9f3781eb22272233e9a76
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-09-12 00:40:06 +0530

    update

--- src/components/circleRating/CircleRating.jsx
+++ src/components/circleRating/CircleRating.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
-import "./circleRating.scss";
+import "./circlerating.scss";
 
 const CircleRating = ({ rating }) => {
   return (
