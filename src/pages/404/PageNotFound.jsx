93f92e61721058f02b3412a66d3ea435c072365e
Pavankumar Guttedar <pavankumar494748@gmail.com>
2023-09-12 00:24:46 +0530

    update

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 import "./pageNotFound.scss";
 
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+
 const PageNotFound = () => {
-  return <div></div>;
+  return (
+    <div className="pageNotFound">
+      <ContentWrapper>
+        <span className="bigText">404</span>
+        <span className="smallText">Page not found!</span>
+      </ContentWrapper>
+    </div>
+  );
 };
 
 export default PageNotFound;
